75028d175a548f813aa2cc3c328bc4dac8de2d02
Taiye Emmanuel <taiyeo@remitly.com>
2020-10-16 16:20:43 +0100

    recyclerview setup

--- app/src/main/java/com/app/taiye/todocleanarchitecture/fragments/list/ListFragment.kt
+++ app/src/main/java/com/app/taiye/todocleanarchitecture/fragments/list/ListFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.app.taiye.todocleanarchitecture.R
+import kotlinx.android.synthetic.main.fragment_list.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -20,7 +22,12 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+      val view  = inflater.inflate(R.layout.fragment_list, container, false)
+      view.floatingActionButton.setOnClickListener {
+          findNavController().navigate(R.id.action_listFragment_to_addFragment)
+      }
+
+        return view
     }
 
 
