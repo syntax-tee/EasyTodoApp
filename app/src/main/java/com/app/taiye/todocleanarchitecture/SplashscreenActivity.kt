3b041cf7861c2c012c7266f6a6c2cc99e9f48c4a
Taiye Emmanuel <taiyeo@remitly.com>
2020-10-20 03:18:41 +0100

    added new fonts

---
+++ app/src/main/java/com/app/taiye/todocleanarchitecture/SplashscreenActivity.kt
@@ -0,0 +1,22 @@
+package com.app.taiye.todocleanarchitecture
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }, DURATION)
+    }
+
+    companion object {
+        const val DURATION = 2000L
+    }
+}
